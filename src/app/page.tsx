0a41a1100c5080b6126f34c2476866a711985e49
Keichan15 <keisuke1515gaming@outlook.jp>
2023-09-30 18:16:22 +0900

    [Add] Cardを使用した大まかなレイアウトイメージの作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,15 @@ import { getList } from "../../libs/microcms";
 
 export default async function Page() {
   const { contents } = await getList();
+  console.log(contents);
 
   if (!contents || contents.length === 0) {
     return <h1>No contents</h1>;
   }
 
   return (
-    <section>
-      <div className="relative px-4 py-12 sm:px-6 lg:py-16 lg:px-8">
+    <>
+      <div className="lg:col-span-2">
         <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
           <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
             Keichan's Tech Blog
@@ -20,12 +21,13 @@ export default async function Page() {
             日々学んだことの備忘録
           </p>
         </div>
-        <div className="relative mx-auto max-w-7xl">
-          <div className="grid max-w-lg gap-5 mx-auto lg:grid-cols-3 lg:max-w-none">
-            {contents.map((post) => {
-              return (
-                // eslint-disable-next-line react/jsx-key
-                <div className="flex flex-col overflow-hidden rounded-lg shadow-lg">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {" "}
+          {contents.map((post) => {
+            return (
+              // eslint-disable-next-line react/jsx-key
+              <div>
+                <Link href={"/articles"}>
                   <div className="flex-shrink-0">
                     <img
                       className="object-cover w-full h-48"
@@ -33,29 +35,28 @@ export default async function Page() {
                       alt=""
                     />
                   </div>
-                  <div className="flex flex-col justify-between flex-1 p-6 bg-white">
-                    <div className="flex-1">
-                      <Link href="#" className="block mt-2">
-                        <p className="text-xl font-semibold text-neutral-600">
-                          {post.title}
-                        </p>
-                        <p className="mt-3 text-base text-gray-500">カテゴリ</p>
-                      </Link>
-                    </div>
-                    <div className="flex items-center mt-6">
-                      <div>
-                        <p className="text-sm font-medium text-gray-500">
-                          タグ
-                        </p>
-                      </div>
+                </Link>
+                <div className="flex flex-col justify-between flex-1 p-6 bg-white">
+                  <div className="flex-1">
+                    <Link href={"/articles"}>
+                      <p className="text-xl font-semibold text-neutral-600">
+                        {post.title}
+                      </p>
+                    </Link>
+                    <p className="mt-3 text-base text-gray-500">カテゴリ</p>
+                  </div>
+                  <div className="flex items-center mt-6">
+                    <div>
+                      <p className="text-sm font-medium text-gray-500">タグ</p>
                     </div>
                   </div>
                 </div>
-              );
-            })}
-          </div>
+              </div>
+            );
+          })}
         </div>
       </div>
-    </section>
+      <div className="lg:col-span-1"></div>
+    </>
   );
 }
