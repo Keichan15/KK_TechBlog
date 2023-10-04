3cca3a8097a578c3649eea3de4207dbae00a5ce8
Keichan15 <keisuke1515gaming@outlook.jp>
2023-10-05 02:28:15 +0900

    コードブロックのファイル名表示

--- src/app/articles/[postId]/page.tsx
+++ src/app/articles/[postId]/page.tsx
@@ -36,6 +36,7 @@ export default async function StaticDetailPage({
   let html = markdownHtml(post.content);
 
   const $ = load(html);
+
   $("pre code").each((_, elm) => {
     const result = hljs.highlightAuto($(elm).text());
     $(elm).html(result.value);
@@ -45,6 +46,15 @@ export default async function StaticDetailPage({
 
   return (
     <div className="lg:col-span-2">
+      <div className="flex flex-wrap w-full mb-20 flex-col items-center text-center">
+        <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
+          Keichan's Tech Blog
+        </h1>
+        <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">
+          日々学んだことの備忘録
+        </p>
+      </div>
+
       <div
         className="markdown"
         dangerouslySetInnerHTML={{ __html: html }}
